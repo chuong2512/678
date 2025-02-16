0acef0587578ce15af8daba0eb263da3c6971f61
pro_c <hieutg244@gmail.com>
2025-02-16 10:17:22 +0700

    update for android

--- Assets/_App/Scripts/IAP Manager/IAPManager.cs
+++ Assets/_App/Scripts/IAP Manager/IAPManager.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections;
-using CI.WSANative.Common;
-using CI.WSANative.Store;
 using SingleApp;
 
 public class IAPKey
@@ -24,20 +22,11 @@ public class IAPManager : PersistentSingleton<IAPManager>
     private bool _isBuyFromShop;
     void Awake()
     {
-        WSANativeCore.Initialise();
     }
    
     //store id get from microsoft partner 
     public void BuyProductID(string storeid, int points = 0)
     {
-        WSANativeStore.RequestPurchase(storeid, result =>
-        {
-            UnityEngine.Debug.Log(result.Status);
-            if(result.Status == WSAStorePurchaseStatus.Succeeded)
-            {
-                // do something here to add point value
-                OnPurchaseSuccess?.Invoke();
-            }
-        });
+       
     }
 }
